98cd2fd9df64301f8cbf1e205da68d2de0bac3cc
JansenQuinten <quiinten@gmail.com>
2017-10-24 13:56:41 +0200

    Tot en met Hoofdstuk 11

--- src/main/java/be/vdab/Main.java
+++ src/main/java/be/vdab/Main.java
@@ -2,14 +2,12 @@ package be.vdab;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import be.vdab.gaming.Spelletje;
+
 class Main {
 	public static void main(String[] args) {
 		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("container.xml")) {
-			ClassA objectA = context.getBean(ClassA.class);
-			System.out.println(objectA.getKoersenURL());
-			System.out.println(objectA.getDefaultTaalEnLand().getDisplayName());
-			System.out.println("Bestand bestaat: " + objectA.getImportData().exists());
-			System.out.println(objectA.getDefaultBladRichting());
+			System.out.println(context.getBean(Spelletje.class).getAantalVijanden());
 		}
 	}
 }
